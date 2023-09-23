b39f724558ab6dede8d16c795029862d9cff3450
lenovo.esucre@gmail.com <felipe3-A>
2023-09-23 10:15:42 -0500

    Creacion de carpetas para modelar

---
+++ src/main/java/conm/co/andrescepeda/userinterfaces/NoticiesInterfaces.java
@@ -0,0 +1,7 @@
+package conm.co.andrescepeda.userinterfaces;
+
+import net.serenitybdd.screenplay.targets.Target;
+
+public class NoticiesInterfaces {
+    public static final Target A_NOTICIES=Target.the("Aceder al modulo de noticias").locatedBy("(//a[@href='/#news-section'])[3]");
+}
---
+++ src/test/java/com/co/andrescepeda/stepsdefinitions/GetNoticiesStepDefinition.java
@@ -0,0 +1,21 @@
+package com.co.andrescepeda.stepsdefinitions;
+
+import cucumber.api.java.en.*;
+
+public class GetNoticiesStepDefinition {
+    @Given("^the user open the page$")
+    public void theUserOpenThePage() {
+
+    }
+
+
+    @When("^he enters the news module$")
+    public void heEntersTheNewsModule() {
+
+    }
+
+    @Then("^he could the news$")
+    public void heCouldTheNews() {
+
+    }
+}
